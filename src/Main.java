72d6974ad33267865d338075e0a52e501976ef23
Dmitry Teterin Qamid-61 <Teterin130215@yandex.ru>
2023-04-16 13:53:38 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int account =100;
+        int payment =1301;
+        int bonus;
+
+        if (payment > 1000) {
+            bonus = payment / 100;
+        }
+        else {
+            bonus = 0;
+        }
+
+        int balance = account + payment + bonus;
+        System.out.println("Здравствуйте, Спасибо что пополнили баланс . Ваш баланс: " + balance + " RUB начислено бонусов : " + bonus + " RUB." );
+    }
+}
